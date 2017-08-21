a7d9aa67521cd9c294b41db61b1c856d6008536e
Alexander Divers <alexanderdivers@Alexanders-MBP.fios-router.home>
2017-08-21 10:11:29 -0400

    adding all burger codes

---
+++ models/burger.js
@@ -0,0 +1,42 @@
+ // * Inside `burger.js`, import `orm.js` into `burger.js`
+
+ //    * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+
+ //    * Export at the end of the `burger.js` file.
+
+
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+
+var burger = 
+{
+
+  selectAll: function(callback)
+  {
+    orm.selectAll(function(res)
+    {
+      callback(res);
+    });
+  },
+
+  insertOne: function(burger_name, callback)
+  {
+    orm.insertOne(burger_name, function(res)
+    {
+      callback(res);
+    });
+  },
+
+  updateOne: function(burger_id, callback)
+  {
+    orm.updateOne(burger_id, function(res)
+    {
+      callback(res);
+    });
+  }
+
+};
+
+
+module.exports = burger;
